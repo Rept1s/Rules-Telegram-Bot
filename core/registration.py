d388bfc57f8047bf623668219e58cc84f4ee30c2
Reptis <167893957+Rept1s@users.noreply.github.com>
2024-06-02 10:59:25 +0300

    update

--- core/registration.py
+++ core/registration.py
@@ -1,14 +1,14 @@
 from aiogram import F
-from core.handlers import check_album_and_send, subscribe_direct_message, start_next_step, access_granted, \
-    rules_step, check_album_and_subscribe
+from core.handlers import subscribe_direct_message, start_next_step, access_granted, \
+    rules_step, AlbumHandler
 from core.filters import FilterChatType, FilterChatAdmin, FilterSenderAnonim, FilterDirectType, FilterSubChannel, \
     FilterUserReg, FilterCallBackChannel, FilterNotUserReg, FilterNotUserRegCallBack, FilterDateBase
 
 
 async def register_reg(dp):
-    dp.message.register(check_album_and_send, FilterNotUserReg(), FilterChatType(), FilterChatAdmin(),
+    dp.message.register(AlbumHandler().check_album_and_send, FilterNotUserReg(), FilterChatType(), FilterChatAdmin(),
                         FilterSenderAnonim())
-    dp.message.register(check_album_and_subscribe, FilterUserReg(), FilterSubChannel(), FilterChatType(),
+    dp.message.register(AlbumHandler().check_album_and_subscribe, FilterUserReg(), FilterSubChannel(), FilterChatType(),
                         FilterChatAdmin(), FilterSenderAnonim())
     dp.message.register(subscribe_direct_message, FilterDirectType(), FilterSubChannel())
     dp.message.register(rules_step, FilterDirectType(), FilterNotUserReg())
